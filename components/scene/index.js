98b17ccdd54ea6e098edbbea4fd2fa2df420541c
Mario Carballo Zama <afk@ellugar.co>
2021-05-05 17:20:16 -0500

    Add new 3D Model animations

--- components/scene/index.js
+++ components/scene/index.js
@@ -1,22 +1,15 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import * as THREE from 'three';
 
 import Logo3D from 'components/logo-3d';
 import styles from './style.module.css';
 
-const bgColor = new THREE.Color('#ffbc2f');
-
 function Scene() {
   return (
     <div className={`${styles['scene-wrapper']}`}>
-      <Canvas
-        onCreated={({ gl }) => {
-          gl.setClearColor(bgColor);
-        }}
-      >
+      <Canvas>
         <Suspense fallback={null}>
-          <Logo3D position={[0, -2.8, 0]} />
+          <Logo3D position={[0, -1, 0]} />
         </Suspense>
       </Canvas>
     </div>
