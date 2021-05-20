f5cafec71a50f072bcf856b5ce3ce065c75f21ef
Mario Carballo Zama <afk@ellugar.co>
2021-05-19 20:50:06 -0500

    remove clip path old polygon

--- components/avatar/index.js
+++ components/avatar/index.js
@@ -11,14 +11,7 @@ function Avatar({ className, flipped, src, alt }) {
   });
   return (
     <div className={customClassName}>
-      <img
-        src={src}
-        alt={alt}
-        className={style['avatar-image']}
-        style={{
-          clipPath: 'path(M.4 0L.1.2 0 .5l.1.3.4.2.4-.2.1-.4L.9.1z)',
-        }}
-      />
+      <img src={src} alt={alt} className={style['avatar-image']} />
       <Mask className={style['avatar-background']} />
       <svg width="0" height="0">
         <clipPath id="svgClip" clipPathUnits="objectBoundingBox">
