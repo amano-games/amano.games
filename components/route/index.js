26e4de9fa6edcec48a336acd4741daab2d940f07
Mario Carballo Zama <afk@ellugar.co>
2021-05-24 13:17:45 -0500

    bugfix: remove route replace url

--- components/route/index.js
+++ components/route/index.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { useRouter } from 'next/router';
 import { useInView } from 'react-intersection-observer';
 import { useWindowSize } from '@reach/window-size';
 
@@ -10,7 +9,6 @@ import NavLink from 'components/nav-link';
 import style from './style.module.css';
 
 function Route({ className, children, href, refId }) {
-  const router = useRouter();
   const { width } = useWindowSize();
   const { ref, inView } = useInView({
     threshold: width > 900 ? 0.4 : null,
@@ -20,12 +18,6 @@ function Route({ className, children, href, refId }) {
     const el = document.getElementById(refId);
     ref(el);
   }, []);
-
-  useEffect(() => {
-    if (inView) {
-      router.replace(href, undefined, { scroll: false, shallow: true });
-    }
-  }, [inView]);
 
   const customClassName = classNames(style.route, 'route', className, {
     [style['-active']]: inView,
