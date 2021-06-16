8143b5328ac167e5470f7f9c9386455f425ce873
Mario Carballo Zama <afk@ellugar.co>
2021-06-16 17:23:02 -0500

    refactor update to next 11

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 
 module.exports = {
   target: 'serverless',
-  future: {
-    webpack5: true,
-  },
+  images: { disableStaticImages: true },
+  webpack5: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
