54081c3220e176371de6ee6ce37bc476d1de4e8b
Mario Carballo Zama <afk@ellugar.co>
2023-04-18 20:45:24 -0600

    feat: add initial support for game details

---
+++ pages/[game-slug]/index.js
@@ -0,0 +1,26 @@
+/* eslint filenames/match-exported: 0 */
+// import { Client } from '@notionhq/client';
+
+import { useRouter } from 'next/router';
+
+import Seo from 'components/seo';
+import Header from 'components/header';
+
+import styles from './game-details.module.css';
+
+export default function GameDetails() {
+  const router = useRouter();
+  const gameSlug = router.query['game-slug'];
+
+  return (
+    <>
+      <Seo />
+      <Header />
+      <section className={styles['game-details-wrapper']}>
+        <header>
+          <h1>{gameSlug}</h1>
+        </header>
+      </section>
+    </>
+  );
+}
