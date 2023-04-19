91f5c4b98cbfae9cd31769afc30abde01f694ffc
Mario Carballo Zama <afk@ellugar.co>
2023-04-19 13:27:22 -0600

    fix: 404 on static paths

--- pages/game/[slug]/wishlist.js
+++ pages/game/[slug]/wishlist.js
@@ -1,15 +1,21 @@
-import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 import { getGames } from 'utils/notion';
 
 import Seo from 'components/seo';
 import Header from 'components/header';
+import Box from 'components/box';
+import Markdown from 'components/markdown';
 
 import styles from './game-details.module.css';
 
-function Wishlist() {
-  const router = useRouter();
-  const gameSlug = router.query.slug;
+function Wishlist({ game }) {
+  useEffect(() => {
+    if (game.wishlist) {
+      window.location.replace(game.wishlist);
+    }
+  }, [game]);
 
   return (
     <>
@@ -17,25 +23,56 @@ function Wishlist() {
       <Header />
       <section className={styles['game-details-wrapper']}>
         <header>
-          <h1>{gameSlug} Wishlist</h1>
+          <h1>{game.name} Wishlist</h1>
         </header>
+        <Box>
+          <Markdown>{`Redirecting to [${game.wishlist}](${game.wishlist}) ...`}</Markdown>
+        </Box>
       </section>
     </>
   );
 }
 
-export async function getServerSideProps(context) {
+Wishlist.propTypes = {
+  game: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    badge: PropTypes.string,
+    subtitle: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    trailer: PropTypes.string,
+    itch: PropTypes.string,
+    newgrounds: PropTypes.string,
+    lexaloffle: PropTypes.string,
+    featured: PropTypes.bool,
+    show_links: PropTypes.bool,
+    wishlist: PropTypes.string,
+  }).isRequired,
+};
+
+export async function getStaticPaths() {
+  const games = await getGames();
+
+  const paths = games.map((item) => {
+    return {
+      params: {
+        slug: item.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    // { fallback: false } means other routes should 404
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
   const { slug } = context.params;
   const games = await getGames();
   const game = games.find((item) => item.slug === slug);
-
-  if (game.wishlist) {
-    return {
-      redirect: {
-        destination: game.wishlist,
-      },
-    };
-  }
 
   return {
     props: { game },
