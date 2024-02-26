7117c8e2d9bfc2f5ced562c02c32c2bcfae3c042
Mario Carballo Zama <afk@ellugar.co>
2024-02-25 20:48:51 -0600

    feat: add changelog page

---
+++ pages/game/[slug]/changelog.js
@@ -0,0 +1,100 @@
+/* eslint filenames/match-exported: 0 */
+import PropTypes from 'prop-types';
+
+import { getChangelog, getGames } from 'utils/notion';
+import { url } from 'lib/site';
+
+import Seo from 'components/seo';
+import Header from 'components/header';
+import Markdown from 'components/markdown';
+
+import styles from './game-changelog.module.css';
+
+function GameDetails({ game, changelog }) {
+  const changelogText = changelog ? changelog.join('\n\n') : null;
+
+  return (
+    <>
+      <Seo
+        title={`${game.name} | Changelog`}
+        description={game.description}
+        image={`${url}/${game.slug}.png`}
+      />
+      <Header />
+      <section className={styles['game-changelog-wrapper']}>
+        <header className={styles['game-changelog-header']}>
+          <div className={`${styles['game-changelog-header-wrapper']} wrapper`}>
+            <h1>{game.name}</h1>
+            <h3>Changelog</h3>
+          </div>
+        </header>
+        <div className={`${styles['game-changelog-content']} `}>
+          <img
+            className={`${styles['game-changelog-cover']}`}
+            src={`/games/${game.slug}.png`}
+            alt={game.name}
+          />
+          {changelogText ? (
+            <Markdown className={`${styles['game-changelog-text']} -inverted`}>
+              {changelogText}
+            </Markdown>
+          ) : null}
+        </div>
+      </section>
+    </>
+  );
+}
+
+GameDetails.propTypes = {
+  changelog: PropTypes.arrayOf(PropTypes.string),
+  game: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    badge: PropTypes.string,
+    subtitle: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    trailer: PropTypes.string,
+    itch: PropTypes.string,
+    newgrounds: PropTypes.string,
+    lexaloffle: PropTypes.string,
+    featured: PropTypes.bool,
+    show_links: PropTypes.bool,
+  }),
+};
+
+GameDetails.defaultProps = {
+  changelog: null,
+  game: {},
+};
+
+export async function getStaticPaths() {
+  const games = await getGames();
+
+  const paths = games.map((item) => {
+    return {
+      params: {
+        slug: item.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    // { fallback: false } means other routes should 404
+    fallback: 'blocking',
+  };
+}
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+  const changelog = await getChangelog(slug);
+  const games = await getGames();
+  const game = games.find((item) => item.slug === slug);
+
+  return {
+    props: { game, changelog },
+  };
+}
+
+export default GameDetails;
--- utils/notion.js
+++ utils/notion.js
@@ -7,6 +7,9 @@ import { parseTableItems } from 'lib/notion';
 export const ABOUTUS_PAGE_ID = '16545b37f4e642a7a4687a2b5e0b9d85';
 export const GAMES_DB_ID = '89794766645149c2a79bb64a72c75d1b';
 export const MANITAS_DB_ID = 'db6aafe3a3c644d3a84a59657f82436a';
+export const CHANGELOGS_IDS = {
+  'pullfrog-playdate-deluxe': '8366bcc2-e5ee-4a49-98ea-25d43279b405',
+};
 
 export function getClient() {
   const notion = new Client({
@@ -48,6 +51,37 @@ export async function getAboutUs() {
   const blocks = data.results.map((block) =>
     richTextToMarkdown(block.paragraph.rich_text)
   );
+
+  return blocks;
+}
+
+export async function getChangelog(slug) {
+  const id = CHANGELOGS_IDS[slug];
+  const notion = getClient();
+  const data = await notion.blocks.children.list({
+    block_id: id,
+    page_size: 50,
+  });
+
+  const blocks = data.results.map((block) => {
+    switch (block.type) {
+      case 'paragraph':
+        return richTextToMarkdown(block.paragraph.rich_text);
+      case 'heading_1':
+        return `# ${richTextToMarkdown(block.heading_1.rich_text)}`;
+      case 'heading_2':
+        return `## ${richTextToMarkdown(block.heading_2.rich_text)}`;
+      case 'heading_3':
+        return `### ${richTextToMarkdown(block.heading_3.rich_text)}`;
+      case 'bulleted_list_item':
+        return `- ${richTextToMarkdown(block.bulleted_list_item.rich_text)}`;
+      case 'divider':
+        return `---`;
+      default:
+        console.warn(`Block type not recognized ${block.type}`);
+        return '';
+    }
+  });
 
   return blocks;
 }
