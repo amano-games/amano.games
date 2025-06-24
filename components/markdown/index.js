22e2f40ccbe9316930eda2188f8bc6b0b18e9cce
afk <afk@ellugar.co>
2025-06-24 14:02:26 -0600

    feat: spoiler tag support

--- components/markdown/index.js
+++ components/markdown/index.js
@@ -3,13 +3,30 @@ import classNames from 'classnames';
 import ReactMarkdown from 'react-markdown';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { gruvboxDark as dark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import remarkGfm from 'remark-gfm';
 
 import RichText from 'components/rich-text';
 
 import style from './style.module.css';
 
+const SPOILER_PREFIX = 'spoiler: ';
+
+function getIsSpoiler(props) {
+  if (props?.node?.tagName !== 'del') {
+    return false;
+  }
+  const { children } = props;
+  const res = String(children).toLowerCase().startsWith(SPOILER_PREFIX);
+  return res;
+}
+
 function renderParagraph(props) {
   const { children } = props;
+  // Can't put a summary inside a <p>
+  // so we need remove the <p> tag if the children is a spoiler.
+  if (getIsSpoiler(children[0]?.props)) {
+    return children;
+  }
   if (
     children &&
     children[0] &&
@@ -22,6 +39,23 @@ function renderParagraph(props) {
 
   return <p>{children}</p>;
 }
+
+function renderSpoiler(props) {
+  const { children } = props;
+  if (!children) return null;
+  const isSpoiler = getIsSpoiler(props);
+  if (isSpoiler) {
+    const child = String(children)?.slice(SPOILER_PREFIX.length);
+    return (
+      <details className={style.spoiler} aria-label="Spoiler">
+        <summary>Spoiler</summary>
+        {child}
+      </details>
+    );
+  }
+  return <del>{children}</del>;
+}
+
 function renderCode({ node, inline, className, children, ...props }) {
   const match = /language-(\w+)/.exec(className || '');
   return !inline && match ? (
@@ -55,9 +89,11 @@ function Markdown({ children, className }) {
     <RichText className={customClassName}>
       <ReactMarkdown
         linkTarget="_blank"
+        remarkPlugins={[remarkGfm]}
         components={{
           p: renderParagraph,
           code: renderCode,
+          del: renderSpoiler,
         }}
       >
         {children}
